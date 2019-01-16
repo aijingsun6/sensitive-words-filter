21f19a9012bc16053a387c1bd25b89bd1ec2dba9
alking <aijingsun6@gmail.com>
2019-01-16 20:03:58 +0800

    NPE need find

--- src/test/java/org/alking/swf/MatchWordTest.java
+++ src/test/java/org/alking/swf/MatchWordTest.java
@@ -27,4 +27,35 @@ public class MatchWordTest {
         Assert.assertEquals("fuck",match.getWord());
 
     }
+
+    @Test
+    public void test2(){
+        DFAFilter dfaFilter = new DFAFilter();
+        dfaFilter.setIgnoreCase(true);
+        dfaFilter.putWord("fuck",1);
+
+        List<DFAMatch> matchList = dfaFilter.matchWord("i fUck u, fuCk r");
+
+        Assert.assertEquals(2,matchList.size());
+        DFAMatch match = matchList.get(0);
+        Assert.assertEquals(2,match.getStart());
+        Assert.assertEquals(5,match.getEnd());
+        Assert.assertEquals("fuck",match.getWord());
+
+        match = matchList.get(1);
+        Assert.assertEquals(10,match.getStart());
+        Assert.assertEquals(13,match.getEnd());
+        Assert.assertEquals("fuck",match.getWord());
+    }
+
+    @Test
+    public void test3(){
+        DFAFilter dfaFilter = new DFAFilter();
+        dfaFilter.setSupportStopWord( true );
+        dfaFilter.setStopWord("|#");
+        dfaFilter.putWord("fuck",1);
+        List<DFAMatch> matchList = dfaFilter.matchWord("i f|u#c|k u");
+        Assert.assertEquals(1,matchList.size());
+
+    }
 }
