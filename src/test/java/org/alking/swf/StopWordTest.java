8161179f7d513e3b1cd2d275cf63737e882dc319
alking <aijingsun6@gmail.com>
2019-01-16 13:35:41 +0800

    add test func

--- src/test/java/org/alking/swf/StopWordTest.java
+++ src/test/java/org/alking/swf/StopWordTest.java
@@ -86,4 +86,68 @@ public class StopWordTest {
         Assert.assertEquals(0,dfaFilter.getcMap().size());
         Assert.assertEquals(0,dfaFilter.getsMap().size());
     }
+
+    @Test
+    public void test5(){
+        DFAFilter dfaFilter = new DFAFilter();
+        dfaFilter.setSupportStopWord( true );
+        dfaFilter.setStopWord("|");
+        dfaFilter.putWord("f|u|c|k", 1);
+
+        Map<Character, DFANode> cMap = dfaFilter.getcMap();
+
+        Assert.assertTrue(cMap.containsKey('f'));
+
+        DFANode node = cMap.get('f');
+        Assert.assertEquals('f', node.c);
+        Assert.assertEquals(0, node.score);
+        Assert.assertFalse(node.leaf);
+
+        node = node.getNode('u');
+        Assert.assertEquals('u', node.c);
+        Assert.assertEquals(0, node.score);
+        Assert.assertFalse(node.leaf);
+
+        node = node.getNode('c');
+        Assert.assertEquals('c', node.c);
+        Assert.assertEquals(0, node.score);
+        Assert.assertFalse(node.leaf);
+
+        node = node.getNode('k');
+        Assert.assertEquals('k', node.c);
+        Assert.assertEquals(1, node.score);
+        Assert.assertTrue(node.leaf);
+    }
+
+    @Test
+    public void test6(){
+        DFAFilter dfaFilter = new DFAFilter();
+        dfaFilter.setSupportStopWord( true );
+        dfaFilter.setStopWord("|# ");
+        dfaFilter.putWord("f u|c#k", 1);
+
+        Map<Character, DFANode> cMap = dfaFilter.getcMap();
+
+        Assert.assertTrue(cMap.containsKey('f'));
+
+        DFANode node = cMap.get('f');
+        Assert.assertEquals('f', node.c);
+        Assert.assertEquals(0, node.score);
+        Assert.assertFalse(node.leaf);
+
+        node = node.getNode('u');
+        Assert.assertEquals('u', node.c);
+        Assert.assertEquals(0, node.score);
+        Assert.assertFalse(node.leaf);
+
+        node = node.getNode('c');
+        Assert.assertEquals('c', node.c);
+        Assert.assertEquals(0, node.score);
+        Assert.assertFalse(node.leaf);
+
+        node = node.getNode('k');
+        Assert.assertEquals('k', node.c);
+        Assert.assertEquals(1, node.score);
+        Assert.assertTrue(node.leaf);
+    }
 }
