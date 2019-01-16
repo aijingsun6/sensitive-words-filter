ccffb4007ce1babf23a8e26a2f2eeb104951bbea
alking <aijingsun6@gmail.com>
2019-01-16 11:53:21 +0800

    test update

--- src/test/java/org/alking/swf/DFAFilterTest.java
+++ src/test/java/org/alking/swf/DFAFilterTest.java
@@ -100,91 +100,9 @@ public class DFAFilterTest {
 
     }
 
-    @Test
-    public void putWordTest3(){
-
-        DFAFilter dfaFilter = new DFAFilter();
-        dfaFilter.setStopWord(false);
-        dfaFilter.setIgnoreCase(false);
-        dfaFilter.setPinyin(true);
-
-        dfaFilter.putWord("毛泽东",1);
-
-        Map<Character,DFANode> cMap = dfaFilter.getcMap();
-        DFANode node = cMap.get('毛');
-        Assert.assertEquals('毛',node.c);
-        Assert.assertEquals(0,node.score);
-        Assert.assertFalse(node.leaf);
-
-        node = node.getNode('泽');
-        Assert.assertEquals('泽',node.c);
-        Assert.assertEquals(0,node.score);
-        Assert.assertFalse(node.leaf);
-
-        node = node.getNode('东');
-        Assert.assertEquals('东',node.c);
-        Assert.assertEquals(1,node.score);
-        Assert.assertFalse(!node.leaf);
-
-        Map<String,DFANode> sMap = dfaFilter.getsMap();
-
-        node =  sMap.get("MAO");
-        Assert.assertEquals('毛',node.c);
-        Assert.assertEquals(0,node.score);
-        Assert.assertEquals("MAO",node.pinyin);
-        Assert.assertFalse(node.leaf);
-
-        node = node.getNode("ZE");
-        Assert.assertEquals('泽',node.c);
-        Assert.assertEquals(0,node.score);
-        Assert.assertEquals("ZE",node.pinyin);
-        Assert.assertFalse(node.leaf);
-
-        node = node.getNode("DONG");
-        Assert.assertEquals('东',node.c);
-        Assert.assertEquals(1,node.score);
-        Assert.assertEquals("DONG",node.pinyin);
-        Assert.assertTrue(node.leaf);
-
-    }
 
     @Test
-    public void putWordTest4(){
-
-        DFAFilter dfaFilter = new DFAFilter();
-        dfaFilter.setStopWord(false);
-        dfaFilter.setIgnoreCase(false);
-        dfaFilter.setPinyin(false);
-
-        dfaFilter.putWord("毛泽东",1);
-
-        Map<Character,DFANode> cMap = dfaFilter.getcMap();
-        Assert.assertEquals(1,cMap.size());
-
-        DFANode node = cMap.get('毛');
-        Assert.assertEquals('毛',node.c);
-        Assert.assertEquals(0,node.score);
-        Assert.assertFalse(node.leaf);
-        Assert.assertEquals(1,node.cMap.size());
-
-        node = node.getNode('泽');
-        Assert.assertEquals('泽',node.c);
-        Assert.assertEquals(0,node.score);
-        Assert.assertFalse(node.leaf);
-        Assert.assertEquals(1,node.cMap.size());
-
-        node = node.getNode('东');
-        Assert.assertEquals('东',node.c);
-        Assert.assertEquals(1,node.score);
-        Assert.assertFalse(!node.leaf);
-        Assert.assertEquals(0,node.cMap.size());
-
-        Map<String,DFANode> sMap = dfaFilter.getsMap();
-        Assert.assertTrue(sMap.isEmpty());
-    }
-
-    @Test
-    public void  putWordTest5(){
+    public void  putWordTest3(){
 
         DFAFilter dfaFilter = new DFAFilter();
         dfaFilter.setStopWord(false);
@@ -196,21 +114,21 @@ public class DFAFilterTest {
         DFANode node = cMap.get('f');
         Assert.assertEquals('f',node.c);
         Assert.assertEquals(1,node.cMap.size());
-        Assert.assertEquals(0,node.sMap.size());
+        Assert.assertNull(node.sMap);
         Assert.assertEquals(0,node.score);
         Assert.assertFalse(node.leaf);
 
         node = node.getNode(' ');
         Assert.assertEquals(' ',node.c);
         Assert.assertEquals(1,node.cMap.size());
-        Assert.assertEquals(0,node.sMap.size());
+        Assert.assertNull(node.sMap);
         Assert.assertEquals(0,node.score);
         Assert.assertFalse(node.leaf);
 
         node = node.getNode('u');
         Assert.assertEquals('u',node.c);
         Assert.assertEquals(1,node.cMap.size());
-        Assert.assertEquals(0,node.sMap.size());
+        Assert.assertNull(node.sMap);
         Assert.assertEquals(0,node.score);
         Assert.assertFalse(node.leaf);
 
@@ -218,14 +136,14 @@ public class DFAFilterTest {
         node = node.getNode(' ');
         Assert.assertEquals(' ',node.c);
         Assert.assertEquals(1,node.cMap.size());
-        Assert.assertEquals(0,node.sMap.size());
+        Assert.assertNull(node.sMap);
         Assert.assertEquals(0,node.score);
         Assert.assertFalse(node.leaf);
 
         node = node.getNode('c');
         Assert.assertEquals('c',node.c);
         Assert.assertEquals(1,node.cMap.size());
-        Assert.assertEquals(0,node.sMap.size());
+        Assert.assertNull(node.sMap);
         Assert.assertEquals(0,node.score);
         Assert.assertFalse(node.leaf);
 
@@ -233,14 +151,14 @@ public class DFAFilterTest {
         node = node.getNode(' ');
         Assert.assertEquals(' ',node.c);
         Assert.assertEquals(1,node.cMap.size());
-        Assert.assertEquals(0,node.sMap.size());
+        Assert.assertNull(node.sMap);
         Assert.assertEquals(0,node.score);
         Assert.assertFalse(node.leaf);
 
         node = node.getNode('k');
         Assert.assertEquals('k',node.c);
-        Assert.assertEquals(0,node.cMap.size());
-        Assert.assertEquals(0,node.sMap.size());
+        Assert.assertNull(node.cMap);
+        Assert.assertNull(node.sMap);
         Assert.assertEquals(1,node.score);
         Assert.assertTrue(node.leaf);
 
